2ea6b527c2a6f99a003e6d114792646fd16de963
geicibarham <geicianecosta43@gmail.com>
2022-01-02 14:35:09 -0500

    first functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an alert! JavaScript is running!");
+function fight() {
+    window.alert("The fight has begun!");
+  }
+  //calling function
+ // fight ();
+
+  //window prompt lets the user insert information-user input//
+  var playerName = window.prompt("What's your robot's name?");
+  //Note the lack of quotation marks around playername
+  console.log(playerName);
+  console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
